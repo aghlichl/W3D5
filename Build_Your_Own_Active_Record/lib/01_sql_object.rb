efc620b510f0812da46683744dff5cca30d527d0
Ari Ghlichloo <aurashghlichloo@gmail.com>
2019-01-25 16:27:26 -0800

    Add Build Your Own ActiveRecord

---
+++ Build_Your_Own_Active_Record/lib/01_sql_object.rb
@@ -0,0 +1,81 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    @data ||= @data = DBConnection.execute2(<<-SQL)
+    SELECT 
+      * 
+    FROM 
+      cats
+    SQL
+
+    @data[0].map(&:to_sym)
+  end
+
+  def self.finalize!
+    columns.each do |col|
+        define_method(col) do 
+          self.instance_variable_get("@#{col}")
+        end 
+        define_method("#{col}=") do |val|
+          self.instance_variable_set("@#{col}", val)
+        end
+    end 
+
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name || self.name.tableize
+  end
+
+  def self.table_name
+    @table_name || self.name.tableize
+  end
+
+  def self.all
+    @data ||= @data = DBConnection.execute(<<-SQL)
+    SELECT 
+      * 
+    FROM 
+      cats
+    SQL
+
+    @data
+    
+  end
+
+  def self.parse_all(results)
+    # ... 
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    # ...
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
